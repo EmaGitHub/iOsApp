1ba22012f85438bf33093c36fa593a6f19258570
Emanuele Calvisi <emanuele.calvisi@eng.it>
2018-07-05 17:08:50 +0200

    HttpViewController created

---
+++ iOsApp/HttpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HttpViewController.swift
+//  iOsApp
+//
+//  Created by Emanuele Calvisi on 05/07/2018.
+//  Copyright © 2018 Emanuele Calvisi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class HttpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
