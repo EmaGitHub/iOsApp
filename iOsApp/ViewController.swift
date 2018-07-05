26acd792fa94bccda1d2d71883722d7c342d607a
Emanuele Calvisi <emanuele.calvisi@eng.it>
2018-07-05 11:44:50 +0200

    Button aHead and second ScreenViewController

--- iOsApp/ViewController.swift
+++ iOsApp/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewHome: UIImageView!
+    
+    @IBOutlet weak var goButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let white = UIColor.white
+        //let red = UIColor.red
+        
+        let lbl = UILabel(frame: CGRect(x:50, y:150, width:200, height:30))
+        lbl.text = "Hi Everyone!!"
+        lbl.textColor = white
+        
+        let img = UIImage(named: "iOs.png")
+        let imageView = UIImageView(image: img)
+        imageView.frame = CGRect(x: 300, y: 100, width: 100, height: 100)
+        
+        self.view.addSubview(imageView)
+        self.view.addSubview(lbl)
     }
 
     override func didReceiveMemoryWarning() {
