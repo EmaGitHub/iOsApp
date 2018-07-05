4e5cc2f6837a4cb56fdc513aada10fbd60579f54
Emanuele Calvisi <emanuele.calvisi@eng.it>
2018-07-05 10:02:55 +0200

    Initial Commit

---
+++ iOsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOsApp
+//
+//  Created by Emanuele Calvisi on 05/07/2018.
+//  Copyright © 2018 Emanuele Calvisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
