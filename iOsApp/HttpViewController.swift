e5307f2a16f0e6bb99674afd8112b729e25eded5
Emanuele Calvisi <emanuele.calvisi@eng.it>
2018-07-12 12:38:38 +0200

    HttpViewController imported Alamofire and init method

--- iOsApp/HttpViewController.swift
+++ iOsApp/HttpViewController.swift
@@ -7,17 +7,14 @@
 //
 
 import UIKit
-import Foundation
+import Alamofire
 
 class HttpViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+                 super.viewDidLoad()
         
-       
-    }
+             }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -34,5 +31,26 @@ class HttpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBOutlet weak var editTextHttp: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func buttonHttpAction(_ sender: UIButton) {
+    
+        let remoteURL = URL(string: editTextHttp.text!)!
+        
+        Alamofire.request(remoteURL).response { response in
+            print("Request: \(response.request)")
+            print("Response: \(response.response)")
+            print("Error: \(response.error)")
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)")
+            }
+            
+            resultLabel!.text = data
+        
+        }
+        
+    }
 }
